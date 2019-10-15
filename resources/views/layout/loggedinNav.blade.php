72be5dc4cfe08f752ad58aca518b90b5cea6dfa8
Heramb <44063354+Hearmb@users.noreply.github.com>
2019-10-15 13:30:43 +0530

    footer

--- resources/views/layout/loggedinNav.blade.php
+++ resources/views/layout/loggedinNav.blade.php
@@ -64,12 +64,14 @@
             </div>
             
             
+<div class="w3-container">
             <div class="logoutBT">
                 <form action="{{ url('/logout') }}" method="get">
                     <button type="submit">Logout</button>
                 </form>
             </div>
         </div>
+                </div>
     </head>
     
     <body>
@@ -80,8 +82,7 @@
             </div>
             <footer>
     <div class="footcontent1">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
+            <p>We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
     </div>
     <div class="footcontent2">
         <h4 class="connectheader">Connect</h4>
