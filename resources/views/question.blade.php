250bf6648734a57b35b95a5f9463d0f8d36218a6
Heramb <44063354+Hearmb@users.noreply.github.com>
2019-10-23 20:40:05 +0530

    navbar

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.navbar')
 
 @section('content')
-<head>
+<!-- <head>
     <style>
         html, body{
   height: 100%;
@@ -16,7 +16,7 @@
          src = "https://code.jquery.com/jquery-2.1.1.min.js"></script>           
       <script src = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js">
       </script>             
-   </head>
+   </head> -->
     
 <div class="container ">
   <div class="section">
